140dadba82ee5c55c7328599ddf703841dd832f5
Rahul Prasad Jaiswal <rahuljaiswal98051@gmail.com>
2025-07-14 06:44:29 +0545

    templeting fix

--- resources/views/backend/includes/mainpanel.blade.php
+++ resources/views/backend/includes/mainpanel.blade.php
@@ -3,12 +3,12 @@
           <div class="row">
             <div class="col-md-12 grid-margin">
               <div class="row">
-                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                <!-- <div class="col-12 col-xl-8 mb-4 mb-xl-0">
                   <h3 class="font-weight-bold">Welcome John</h3>
                   <h6 class="font-weight-normal mb-0">All systems are running smoothly! You have <span
                       class="text-primary">3 unread alerts!</span></h6>
-                </div>
-                <div class="col-12 col-xl-4">
+                </div> -->
+                <!-- <div class="col-12 col-xl-4">
                   <div class="justify-content-end d-flex">
                     <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
                       <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button" id="dropdownMenuDate2"
@@ -22,11 +22,11 @@
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
+                </div> -->
+              <!-- </div>
             </div>
-          </div>
-          <div class="row">
+          </div> -->
+          <!-- <div class="row">
             <div class="col-md-6 grid-margin stretch-card">
               <div class="card tale-bg">
                 <div class="card-people mt-auto">
@@ -87,8 +87,8 @@
                 </div>
               </div>
             </div>
-          </div>
-          <div class="row">
+          </div> -->
+          <!-- <div class="row">
             <div class="col-md-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
@@ -103,8 +103,8 @@
                 </div>
               </div>
             </div>
-          </div>
-          <div class="row">
+          </div> -->
+          <!-- <div class="row">
             <div class="col-md-12 grid-margin stretch-card">
               <div class="card position-relative">
                 <div class="card-body">
@@ -326,8 +326,8 @@
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-md-12 grid-margin stretch-card">
+          <div class="row"> -->
+            <!-- <div class="col-md-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
                   <p class="card-title mb-0">Top Products</p>
@@ -405,9 +405,9 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> -->
         <!-- content-wrapper ends -->
         <!-- partial:partials/_footer.html -->
-       @includes('backend.includes.footer')
+       <!-- @includes('backend.includes.footer') -->
         <!-- partial -->
       </div>
--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,6 @@ Route::view('/login', 'frontend.login')->name('login');
 Route::post('/signup-submit', [UserController::class, 'signup'])->name('signup-submit');
 Route::post('/login-submit', [UserController::class, 'login'])->name('login-submit');
 
-route::view('/dashboard', 'backend.dashboard')->name('dashboard');
+
+// Backend Route
+Route::view('/dashboard', 'backend.dashboard')->name('dashboard');
